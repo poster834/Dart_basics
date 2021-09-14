fa9710dab0ab2a2313fa326de67b0449a68ad931
poster834 <poster834@gmail.com>
2021-09-14 08:19:41 +0300

    add 2 func (int-binary)

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,5 +1,46 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basics.calculate()}!');
+void main() {
+  var nc = NumberConvert();
+  print(nc.intToBinary(95));
+  print(nc.binaryToInt('1011111'));
+}
+
+// Реализуйте методы вычисления НОД и НОК целых чисел. Реализуйте метод, который разбивает число на простые множители и возвращает их.
+
+// Реализуйте методы для преобразования целых чисел из десятичной системы в двоичную и обратно.
+class NumberConvert {
+  int binaryToInt(String strNum) {
+    int intNum = 0;
+    List strlist = strNum.split('');
+    int j = -1;
+    for (var i = strlist.length - 1; i >= 0; i--) {
+      j++;
+      int n = int.parse(strlist[i]);
+      if (n > 0) {
+        intNum = intNum + (pow(2, j)).toInt();
+      }
+    }
+    return intNum;
+  }
+
+  String intToBinary(var num) {
+    List<String> binaryArr = [];
+    while (num >= 1) {
+      if ((num % 2) == 0) {
+        binaryArr.add('0');
+        num = num ~/ 2;
+      }
+      if ((num % 2) != 0) {
+        binaryArr.add('1');
+        num = num ~/ 2;
+      }
+    }
+    String binNum = "";
+    for (var i = binaryArr.length - 1; i >= 0; i--) {
+      binNum += binaryArr[i];
+    }
+    return binNum;
+  }
 }
